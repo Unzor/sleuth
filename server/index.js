66d5a9d730328324152bb379f00d6bcd7ef5144c
Unzor <71789103+Unzor@users.noreply.github.com>
2022-04-07 18:23:01 -0500

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const express = require('express');
+const app = express();
+ 
+app.use(express.json({limit: "100mb"}));
+app.use(require("express-all-allow")())
+ 
+if (!fs.existsSync("sleuthfs/")) {
+  fs.mkdirSync("sleuthfs");
+}
+ 
+app.post('/sleuthfs/send', (req, res) => {
+  var file = decodeURIComponent(escape(atob(req.body.file)));
+  fs.writeFileSync("sleuthfs/" + file.split("|")[0], Buffer.from(Uint8Array.from(eval("[" + file.split("|")[1] + "]")).buffer));
+  res.send({success: true});
+});
+ 
+app.listen(3000, () => {
+  console.log('server started');
+});
