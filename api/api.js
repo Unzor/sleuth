d820bb60663f360ce0a2a51a75659be58a8cbe39
Unzor <71789103+Unzor@users.noreply.github.com>
2022-04-09 12:44:55 -0500

    Create api.js

---
+++ api/api.js
@@ -0,0 +1,37 @@
+class Sleuth {
+    constructor(settings) {
+        this.protocol = settings.protocol || "http";
+        this.host = settings.host
+        this.port = settings.port ? ":" + settings.port : ":3000"
+    }
+    send(settings) {
+        var the = this;
+        var fs;
+        var GetFile = new FileReader();
+
+        GetFile.onload = function() {
+            fetch(the.protocol + "://" + the.host + the.port + "/sleuthfs/send", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    file: btoa(unescape(encodeURIComponent(settings.filename + "|" + new TextEncoder("utf-8").encode(GetFile.result).toString())))
+                })
+            })
+        }
+        GetFile.readAsText(settings.file);
+
+    }
+}
+
+// Example:
+/*
+var host = new Sleuth({
+    host: "127.0.0.1"
+});
+host.send({
+    file: document.querySelector("#file").files[0],
+    filename: "file.exe"
+})
+*/
