46a6910d1dfd8a578e112fa084d842c83a35ea48
Unzor <71789103+Unzor@users.noreply.github.com>
2022-04-09 22:50:02 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 var cli2json = require("cli2json");
 var colors = require('@colors/colors');
 var options = cli2json.parse(process.argv.slice(2).join(" "), {
-    readCommandAfter: ["-p", "--port", "-l", "--limit"]
+  readCommandAfter: ["-p", "--port", "-l", "--limit"]
 })
 const fs = require("fs");
 const express = require('express');
 const app = express();
-
+ 
 var arrayFindIncludes = function(r, n) {
     var u;
     var a = null;
@@ -20,59 +20,52 @@ var arrayFindIncludes = function(r, n) {
     });
     return u
 }
-
 function dataurl_to_file(dataurl) {
-    var regex = /^data:.+\/(.+);base64,(.*)$/;
-    var matches = dataurl.match(regex);
-    var data = matches[2];
-    return Buffer.from(data, 'base64');
+var regex = /^data:.+\/(.+);base64,(.*)$/;
+var matches = dataurl.match(regex);
+var data = matches[2];
+return Buffer.from(data, 'base64');
 }
 
-var limit = {
-    limit: arrayFindIncludes("-l", options.flags) || arrayFindIncludes("--limit", options.flags) || "250mb"
-};
+var limit = {limit: arrayFindIncludes("-l", options.flags) ||arrayFindIncludes("--limit", options.flags) ||"250mb"};
 
 if (limit.limit.startsWith("-l") || limit.limit.startsWith("--limit")) {
-    limit = limit.limit.split(" ")[1];
+	limit = limit.limit.split(" ")[1];
 }
 
 
-app.use(express.json({
-    limit: limit
-}));
+app.use(express.json({limit: limit}));
 
 
 app.use(require("express-all-allow")())
-
+ 
 if (!fs.existsSync("sleuthfs/")) {
-    fs.mkdirSync("sleuthfs");
+  fs.mkdirSync("sleuthfs");
 }
-
+ 
 app.post('/sleuthfs/send', (req, res) => {
-    var file = req.body.file;
-    if (!fs.existsSync("sleuthfs/")) {
-        fs.mkdirSync("sleuthfs");
-    }
+	var file = req.body.file;
+	if (!fs.existsSync("sleuthfs/")) {
+  fs.mkdirSync("sleuthfs");
+  }
 
-    var data = dataurl_to_file(file.split("|")[1])
+var data = dataurl_to_file(file.split("|")[1])
 
-    fs.writeFileSync("sleuthfs/" + file.split("|")[0], data);
-    console.log(file.split("|")[0].green + " is being added to your Sleuth directory.\nCheck C:\\Program Files\\sleuth\\sleuthfs to see the file.")
-    res.send({
-        success: true
-    });
+  fs.writeFileSync("C:\\Program Files\\sleuth\\sleuthfs\\" + file.split("|")[0], data);
+  console.log(file.split("|")[0].green + " is being added to your Sleuth directory.\nCheck C:\\Program Files\\sleuth\\sleuthfs to see the file.")
+  res.send({success: true});
 });
-
+ 
 var port = arrayFindIncludes("-p", options.flags) || arrayFindIncludes("--port", options.flags) || 3000
 
 if (port.startsWith) {
-    if (port.startsWith("-p") || port.startsWith("--port")) {
-        port = port.split(" ")[1];
-    }
+if (port.startsWith("-p") || port.startsWith("--port")) {
+	port = port.split(" ")[1];
+}
 }
 
 app.listen(port, () => {
-    console.log(`  ${'Sleuth v1.1.3'.yellow}
+  console.log(`  ${'Sleuth v1.1.3'.yellow}
   ${'Server started!'.green} 
   To let people connect to your server, link them here with your port-forwarded public IP address along with
   the port, which is ${port.toString().cyan}:
